5d0507b93f063e2dae623d168e99c33ea009c300
lenzzair <lenny.lambert4@gmail.com>
2025-03-13 16:28:45 +0100

    perf(client): :lock: Add error gestion
    Add gestion error with try -> exept for raise any socket error

--- client.py
+++ client.py
@@ -12,20 +12,44 @@ class Client:
         
     
     def Run_client(self):
-        tcp_socket = socket.socket(Client.Type_Ipv4, Client.Type_TCP)
         
+        try:
+            tcp_socket = socket.socket(Client.Type_Ipv4, Client.Type_TCP)
+            
+        except socket.error as e:
+          print(f"Une erreur est survenue =====> {e}")
+          exit()
+            
         Address = (self.ip_server, self.port_ecoute)
-        tcp_socket.connect(Address)
-
-        tcp_socket.send(self.message.encode("utf8"))
-        print("Message envoyer")
         
-        data = tcp_socket.recv(Client.Taille_Bit)
+        try:
+            tcp_socket.connect(Address)
+            
+        except socket.error as e:
+            print(f"Une erreur est survenue =====> {e}")
+            exit()
+          
+        try:
+          tcp_socket.send(self.message.encode("utf8"))
+          
+        except socket.error as e:
+            print(f"Une erreur est survenue =====> {e}")
+            exit()
+    
+    
+        print("Message envoyer a serveur")
+        
+        if not data:
+            print("Une erreur lors de la reception de la réponse du serveur")
+            exit()
+            
+        else:
+            data = tcp_socket.recv(Client.Taille_Bit)
         
         tcp_socket.close()
         
         print(f"Réponse serveur {data.decode()}")
        
-        
+
 client1 = Client("192.168.1.34", 2000, "Hello sale neuil")
 client1.Run_client()
