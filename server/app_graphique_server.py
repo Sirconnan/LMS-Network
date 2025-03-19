0ff4f411860fbe2ba3507626e214e04b487c34ce
Sirconnan <mariet.medrick@gmail.com>
2025-03-19 14:58:42 +0100

    refactor(server): :construction_worker:

--- server/app_graphique_server.py
+++ server/app_graphique_server.py
@@ -4,6 +4,7 @@ from server import Server
 from tkinter import * 
 from tkinter.messagebox import * 
 
+
 class stdout_redirector: # ====> Create class for redirect the stdout flux
     # => This class replace the method sys.stdout because the function print() is
     # a compat of sys.stdout.write. So we rempalce sys.stdout with this class for
@@ -14,12 +15,11 @@ class stdout_redirector: # ====> Create class for redirect the stdout flux
 
     def write(self, message): # ====> Override the method write for write in the Tkinter text widget
         self.text_widget.insert(END, message) # => Insert at the end of the object text
-        self.text_widget.see(END) # => Scroll a the end automatically
-
-    def flush(self): # ====> For app use the method sys.stdout.flush
-        pass
+        self.text_widget.see(END) # => Scroll a the end automatically   
 
 class Interface:
+
+    __ip_list_use = []
     __ip = socket.gethostbyname(socket.gethostname())
     __nb_server = 0
 
@@ -47,12 +47,12 @@ class Interface:
         server_gui = Toplevel(self.gui)
         server_gui.configure(bg='gray25')
 
-        ip_lablel = Label(server_gui, text="IP of the server").pack()
+        Label(server_gui, text="IP of the server").pack()
         ip_input = Entry(server_gui)
         ip_input.pack()
         ip_input.insert(0, Interface.__ip)
 
-        port_lablel = Label(server_gui, text="Port of the server").pack()
+        Label(server_gui, text="Port of the server").pack()
         port_input = Spinbox(server_gui, from_=49152, to=65535)
         port_input.pack()
 
@@ -66,6 +66,10 @@ class Interface:
                 showerror("Error IP", "Invalid format the IP. Expected format : \"127.0.0.1\"")
                 return
             
+            if ip in Interface.__ip_list_use:
+                showerror("Error IP", "IP already used")
+                return
+            
             if not port.isdigit():
                 showerror("Error Port", "Inva+lid format the port. Expected format : \"52152\"")
                 return
@@ -77,19 +81,19 @@ class Interface:
                 return
 
             if askyesno("Checking information", f"Are you sure of connect information:\n- IP: {ip}\n- Port: {port}"):
-                
+                Interface.__ip_list_use.append(ip)
                 for widget in server_gui.winfo_children():
                     widget.destroy()
                 
                 Label(server_gui, text=f"Server started at {ip} on port {port}").pack()
 
-                frame_stdout = Frame(server_gui)
-                frame_stdout.pack(fill=BOTH, expand=True)
+                frame_stdout_server = Frame(server_gui)
+                frame_stdout_server.pack(fill=BOTH, expand=True)
 
-                text_stdout = Text(frame_stdout, wrap="word", height=10, width=50)
-                text_stdout.pack(fill=BOTH, expand=True)
+                text_stdout_server = Text(frame_stdout_server, wrap="word", height=10, width=50)
+                text_stdout_server.pack(fill=BOTH, expand=True)
 
-                sys.stdout = stdout_redirector(text_stdout) # => remplace the method of sys
+                sys.stdout = stdout_redirector(text_stdout_server) # => remplace the method of sys
 
                 def run_server():
                     server_i = Server(ip, port)
@@ -102,8 +106,19 @@ class Interface:
 
 
 
-tmp = Interface(150, 50, "Server App")
+tmp = Interface(1000, 1000, "Server App")
 
 Button(tmp.gui, text="New server", command=tmp.new_server).pack()
+
+frame_stdout_main = Frame(tmp.gui)
+frame_stdout_main.pack(fill=BOTH, expand=True)
+
+text_stdout_main = Text(frame_stdout_main, wrap="word", height=10, width=50)
+text_stdout_main.pack(fill=BOTH, expand=True)
+
+sys.stdout = stdout_redirector(text_stdout_main)
+sys.stderr = stdout_redirector(text_stdout_main)
+
+
 
 tmp.run()
