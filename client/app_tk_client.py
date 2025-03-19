19efd9678512c91c56f8c70d02d15eccc33ab286
lenzzair <lenny.lambert4@gmail.com>
2025-03-19 14:56:05 +0100

    refactor(client): :construction: Work with interface graphic

--- client/app_tk_client.py
+++ client/app_tk_client.py
@@ -6,35 +6,30 @@ class Lms_network:
     
     def __init__(self):
         self.gui = tk.Tk()
-        
-    
-    def run_graphique(self):
         self.gui.config(bg='skyblue')
         self.gui.title("LMS Network")
         self.gui.geometry("1000x500")
+    
+    def run_graphique(self):
         self.gui.mainloop()
         
-    def start_new_client():
+    def start_new_client(self):
         Lms_network.__nb_client += 1
-        
-        numero_client = Lms_network.__nb_client
-        instance_client = Lms_network()
-        instance_client.run_graphique()
-        
-        
-        label_def_title = tk.Label(self.gui, 
+    
+        instance_client = tk.Toplevel(self.gui)
+
+        label_def_title = tk.Label(instance_client, 
                                text="Veuillez choisir une adresse IP ou un nom de domaine du serveur cible :", 
                                bg='skyblue', 
-                               font=("Arial", 14))
-        label_def_title.pack(pady=20)
+                               font=("Arial", 14)).pack(pady=20)
+        
 
        
-        self.label_def_input = tk.Entry(self.gui, font=("Arial", 14), width=50)
-        self.label_def_input.pack(pady=10)
+        self.input_start = tk.Entry(instance_client, font=("Arial", 14), width=50)
+        self.input_start.pack(pady=10)
 
 
-        submit_button = tk.Button(self.gui, text="Valider", command=self.get_input, font=("Arial", 12))
-        submit_button.pack(pady=20)
+        submit_button = tk.Button(instance_client, text="Valider", command=self.get_input, font=("Arial", 12)).pack(pady=20)
         
 
     def get_input(self):
@@ -42,5 +37,5 @@ class Lms_network:
         print(f"Adresse saisie : {user_input}")  
     
 instance1 = Lms_network()
-tk.Button(instance1.gui, text="Crée un nouveaux client", command=Lms_network.start_new_client()).pack(pady=20)
+tk.Button(instance1.gui, text="Crée un nouveaux client", command=instance1.start_new_client).pack(pady=20)
 instance1.run_graphique()
